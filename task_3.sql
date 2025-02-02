63b4818ce24f0b99c0997fd3c21901d977e8df16
Limah Temitope <limahtechnology@yahoo.com>
2025-02-02 13:19:38 +0100

    Task3: Sql command to show all tables in the alx_book_store database.

---
+++ task_3.sql
@@ -0,0 +1,14 @@
+import mysql.connector, os
+from dotenv import load_dotenv
+load_dotenv()
+PASSWORD = os.environ.get("PASSWORD")
+
+mydb = mysql.connector.connect(host="localhost", 
+                               user="root",
+                               password=PASSWORD,
+                               database = "alx_book_store"
+                               )
+
+cursor = mydb.cursor()
+cursor.execute("show tables")
+print(cursor.fetchall())
